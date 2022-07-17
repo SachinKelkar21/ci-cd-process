5d1784623aebf5d024d834826cf6bbf2159cc1a6
SachinKelkar21 <sachin.kelkar79@gmail.com>
2022-07-18 00:56:36 +0530

    Initialize application

---
+++ src/test/java/com/cicdprocess/cicdprocess/CiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdprocess.cicdprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
